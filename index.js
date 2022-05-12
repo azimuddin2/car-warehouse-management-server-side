06321db47abfb67736685b0d14a747073613eb9d
azimuddin2 <mohammadazimuddin274@gmail.com>
2022-05-12 19:54:14 +0600

    quantity delete item servercode add

--- index.js
+++ index.js
@@ -5,11 +5,9 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-
 // middleware
 app.use(cors());
 app.use(express.json());
-
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.iohgz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
@@ -60,7 +58,7 @@ async function run() {
                     quantity: updatedQuantity.updatedQuantity
                 },
             };
-            const result = await cycleCollection.updateOne(
+            const result = await itemCollection.updateOne(
                 filter,
                 updateDoc,
                 options
@@ -68,6 +66,28 @@ async function run() {
             console.log(result)
             res.send(result);
         });
+
+        app.put('/item/:id', async (req, res) => {
+            const id = req.params.id;
+            const updatedQuantity = (req.body)
+            console.log(req.body)
+            const filter = {_id: ObjectId(id)};
+            const options = { upsert: false };
+            const updateDoc = {
+                $set: {
+                    quantity: updatedQuantity.updatedQuantity
+                },
+            };
+            const result = await itemCollection.updateOne(
+                filter,
+                updateDoc,
+                options
+            );
+            console.log(result)
+            res.send(result);
+        });
+
+        
     }
     finally {
 
